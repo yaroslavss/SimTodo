912ebc0e2ea690aae712a58fe8535762d7e8e48b
yaroslavss <yaroslav.syuzev@gmail.com>
2024-01-15 20:59:26 +0400

    Add fields to layout and view model to create new fragment

--- app/src/main/java/ru/yara/simtodo/ui/addevent/AddEventFragment.kt
+++ app/src/main/java/ru/yara/simtodo/ui/addevent/AddEventFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import ru.yara.simtodo.R
+import ru.yara.simtodo.data.db.EventEntity
 import ru.yara.simtodo.databinding.FragmentAddEventBinding
-import java.time.format.DateTimeFormatter
 
 class AddEventFragment : Fragment() {
 
     private var _binding: FragmentAddEventBinding? = null
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(AddEventViewModel::class.java)
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,6 +29,17 @@ class AddEventFragment : Fragment() {
 
         _binding = FragmentAddEventBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // add event
+        val navController = findNavController()
+        binding.btnAddEvent.setOnClickListener {
+            viewModel.addEventIntoDB(EventEntity())
+            navController.navigate(R.id.homeFragment)
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/ru/yara/simtodo/ui/addevent/AddEventViewModel.kt
@@ -0,0 +1,11 @@
+package ru.yara.simtodo.ui.addevent
+
+import androidx.lifecycle.ViewModel
+import ru.yara.simtodo.data.db.EventEntity
+
+class AddEventViewModel : ViewModel() {
+
+    fun addEventIntoDB(eventEntity: EventEntity) {
+
+    }
+}
